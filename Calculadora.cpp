015ecc5b9f2c0bce0420a0dc7b5b87f48a297197
Martin Garcia Hurtado <m.garciahurtado@ugto.mx>
2019-06-18 09:23:36 -0500

    Menu

---
+++ Calculadora.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double coseno(int);
+coseno(int a){
+	
+}
+int main(){
+	int op=0,n,o;
+	
+	while(op!=0){
+		cout<<"Que funcion desea realizar."<<endl
+			<<"1-.Seno."<<endl
+			<<"2-.Coseno."<<endl
+			<<"3-.Tangente."<<endl
+			<<"4-.Arco Seno."<<endl
+			<<"5-.Arco Coseno."<<endl
+			<<"6-.Arco Tangente."<<endl:
+		cin>>o;
+		
+		switch(o){
+			case 1:
+				
+				break;
+			case 2:
+				
+				break;
+			case 3:
+				
+				break;
+			case 4:
+				
+				break;
+			case 5:
+				
+				break;
+			case 6:
+				
+				break;
+		}
+	} 
+	return 0;
+}
